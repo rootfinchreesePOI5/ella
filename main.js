23d4b77a5f61675fa09e51230a66bcbf261ad2c6
rootfinchreesePOI5 <mohamedabdiaziz515@gmail.com>
2024-10-05 15:06:29 +0300

    first

---
+++ main.js
@@ -0,0 +1,41 @@
+document.getElementById("button").addEventListener("click", function() {
+    let dropdown = document.getElementById("dropdown");
+    dropdown.classList.toggle("show"); // Toggles the 'show' class
+    let originalBtn = document.getElementById("button")
+    originalBtn.style.display = "none"
+});
+document.getElementById("back").addEventListener("click", () => {
+    let dropdown = document.getElementById("dropdown");
+    let originalBtn = document.getElementById("button");
+    dropdown.classList.remove("show");
+    originalBtn.style.display = "block"
+})
+
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+let sections = document.querySelectorAll('section');
+let navlinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () =>{
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height){
+            navlinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ' ]').classList.add('active')
+            })
+        }
+    })
+}
+
+
+
+menuIcon.onclick = () =>{
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+
